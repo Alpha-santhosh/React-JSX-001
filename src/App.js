ae8480b192da1f0000d8a0157127d052d273a42f
santhosh s <subramanisanthosh00@gmail.com>
2023-02-17 14:22:29 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>About Us</h2>
+      <p>at Zappos.com, our purpose is simple: to live and devliver WOW</p>
+      <p>Twenty years ago, we began as a small online retailer that only sold shoed. Today, we sell shoes--as well as clothing, handbags, accessories and more.That "more" is providing the very best customer service, customer experience and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers, employees, vendors, shareholders and the community in a long-term sustainable way.</p>
+      <p>We hope that in the future people won't even realize we started selling shoes online.instead they'll know Zappos as a service company that just happens to sell_____.</p>
     </div>
   );
 }
